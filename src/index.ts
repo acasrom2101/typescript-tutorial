d6875318f2827e26e88f7709c6192b19c3511df6
Alfonso Casanueva <acasrom2101@iescarrillo.es>
2025-09-29 08:48:01 +0200

    Añadidos ejemplos de sintaxis básica para TypeScript y modificación de .gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,53 @@
-let x = 1
-console.log("Hola2");
+//Declaración de variables
+const z = "Fran";
+if(true){
+    var x="Maria";
+    let y="Jose"; //y tiene ámbito de bloque
+}
+
+x="Pepito"; //x tiene un ámbito global, podemos modificar el valor del if
+console.log(x);
+
+var y;
+
+//const z=1; Deben estar inicializadas si o si y no se pueden cambiar
+
+/**
+ * Primitivos
+ * 
+ * string
+ * number (incluye decimales)
+ * boolean
+ * 
+ */
+
+let nombre:string = "Jose Antonio";
+let edad:number = 18;
+let mayorEdad:boolean = true; //También vale mayor_edad
+
+/**
+ * Datos especiales
+ * 
+ * any
+ * unknown
+ * void
+ * 
+ */
+
+//Any provocaría un error en tiempo de compilación
+let cualquierCosa:any = "Hola";
+cualquierCosa=2;
+cualquierCosa=true;
+
+//Uso del tipo unknown
+let tipoDatoDesconocido:unknown;
+
+tipoDatoDesconocido = "Nombre";
+tipoDatoDesconocido = 18;
+
+if (typeof(tipoDatoDesconocido) == "string"){
+    console.log(tipoDatoDesconocido.toUpperCase());
+}
+
+console.log("Tu nombre es "+nombre);
+console.log(`Your next line is ${nombre}`)
